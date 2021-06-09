7a73bd9f9f509e2eafe7f844aee72dd3b4de26ae
Javiistacks <javiann.marrero@icloud.com>
2021-06-09 15:26:08 -0500

    connected atlas database

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/dbBudgetTracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
